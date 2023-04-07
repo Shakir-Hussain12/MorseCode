d994635f8c524976d744f8a9845439a309d32d4a
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-04-08 00:42:32 +0500

    Added Data Back to the Right Branch

---
+++ morse.rb
@@ -0,0 +1,46 @@
+M_CODES = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
+def char_splitter(words)
+  converted = []
+  i = 0
+  words.map do |w|
+    chars = w.split
+    test = []
+    chars.each do |c|
+      test.push(M_CODES[c])
+    end
+    test = test.join
+    converted[i] = test
+    i += 1
+  end
+  converted.join(' ')
+end
+
+# Add code here
